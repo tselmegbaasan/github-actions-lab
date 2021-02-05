62315411ae8ee184ae067e713ba26693af6cc81e
Tselmeg Baasan <37698237+tselmegbaasan@users.noreply.github.com>
2021-02-05 15:52:03 +0100

    Update entrypoint.sh

--- actions-a/entrypoint.sh
+++ actions-a/entrypoint.sh
@@ -1,2 +1,52 @@
-#!/bin/sh -l
 
+#!/usr/bin/env ruby
+
+require "json"
+require "octokit"
+
+json = File.read(ENV.fetch("GITHUB_EVENT_PATH"))
+event = JSON.parse(json)
+
+github = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
+
+if !ENV["GITHUB_TOKEN"]
+  puts "Missing GITHUB_TOKEN"
+  exit(1)
+end
+
+if ARGV[0].empty?
+  puts "Missing message argument."
+  exit(1)
+end
+
+message = ARGV[0]
+check_duplicate_msg = ARGV[1]
+repo = event["repository"]["full_name"]
+
+if ENV.fetch("GITHUB_EVENT_NAME") == "pull_request"
+  pr_number = event["number"]
+else
+  pulls = github.pull_requests(repo, state: "open")
+
+  push_head = event["after"]
+  pr = pulls.find { |pr| pr["head"]["sha"] == push_head }
+
+  if !pr
+    puts "Couldn't find an open pull request for branch with head at #{push_head}."
+    exit(1)
+  end
+  pr_number = pr["number"]
+end
+
+coms = github.issue_comments(repo, pr_number)
+
+if check_duplicate_msg == "true"
+  duplicate = coms.find { |c| c["body"] == message }
+
+  if duplicate
+    puts "The PR already contains this message"
+    exit(0)
+  end
+end
+
+github.add_comment(repo, pr_number, message)
